d9be362d2440c87a1d681f934dbecbd7c9017262
LndnMdiaLb <projects@thelondonmedialab.com>
2019-05-17 14:30:36 +0100

    Babel Loader / @babel/preset-react
    Babel offers transpiling functionality.  In this case translating JSX syntax.  Removind the babel-loader module
    entirely creates a basic dependency graph.  By adding the loader and preset-react the webpack bundler will replace </>
    with calls to .createElemnt

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path= require('path') ;
 
-
 const createEntry=name=>path.resolve(__dirname, './src', `${name}.js`)
 const createOutputPath=name=>path.resolve(__dirname, './dist')
 
-
 module.exports=(env , argv)=>({
 
-    entry: createEntry('i1') ,
+    entry: [ createEntry('i1'), createEntry('i2') ],
 
     output: {
         path: createOutputPath() ,
@@ -17,7 +15,12 @@ module.exports=(env , argv)=>({
     module:{
         rules:[
             {
-                use: 'babel-loader' ,
+                use: {
+                    loader:'babel-loader' ,
+                    options: {
+                        presets:["@babel/preset-react"]
+                    }
+                },
                 test: /\.m?js$/ ,
                 exclude: /(node_modules|bower_components)/
             }
