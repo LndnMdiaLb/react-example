ab03e6f3f954fedec79134b3606df4b9055712b3
LndnMdiaLb <projects@thelondonmedialab.com>
2019-05-28 19:30:49 +0100

    create data mock

---
+++ src/data/data.js
@@ -0,0 +1,62 @@
+export const data={
+
+    'jbjerlw':{
+        thumbnail:'https://placeimg.com/160/240/any',
+        title:'Ghostbusters II',
+        showtimes:['16:45','18:35'] ,
+        img:'https://placeimg.com/640/480/any',
+        synopsis:'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis felis felis. Maecenas porta feugiat massa, at sagittis lectus facilisis vel. Vestibulum sit amet dolor maximus mauris posuere accumsan ut vulputate odio. Phasellus ut ipsum quis ex congue auctor. Proin id varius sapien, ut dictum lacus. Morbi ac ullamcorper nibh. Vivamus rhoncus est sed lorem tristique, id pharetra ante maximus. Mauris nunc justo, bibendum eu tempor id, ultrices eu ipsum. Curabitur placerat magna ut leo consequat, nec consectetur neque sodales. Curabitur eget risus tellus. Etiam hendrerit mi in dictum tincidunt. Nulla vulputate mi sem, id rutrum diam viverra quis. Cras eros lectus, dapibus nec lacinia vel, convallis a libero. Aliquam vel mauris faucibus, accumsan justo ut, ultrices lectus. Cras condimentum neque a mauris egestas, vitae convallis mi pellentesque. Integer cursus tellus tortor, vel dictum odio placerat vel. Morbi vehicula leo nisl, et porttitor diam sagittis ut.',
+        rating:4,
+        similar:['skjebwn']
+    } ,
+
+    'skjebwn':{
+        thumbnail:'https://placeimg.com/160/240/any',
+        title:'Inception',
+        showtimes:['16:45','18:35'] ,
+        img:'https://placeimg.com/640/480/any',
+        synopsis:'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis felis felis. Maecenas porta feugiat massa, at sagittis lectus facilisis vel. Vestibulum sit amet dolor maximus mauris posuere accumsan ut vulputate odio. Phasellus ut ipsum quis ex congue auctor. Proin id varius sapien, ut dictum lacus. Morbi ac ullamcorper nibh. Vivamus rhoncus est sed lorem tristique, id pharetra ante maximus. Mauris nunc justo, bibendum eu tempor id, ultrices eu ipsum. Curabitur placerat magna ut leo consequat, nec consectetur neque sodales. Curabitur eget risus tellus. Etiam hendrerit mi in dictum tincidunt. Nulla vulputate mi sem, id rutrum diam viverra quis. Cras eros lectus, dapibus nec lacinia vel, convallis a libero. Aliquam vel mauris faucibus, accumsan justo ut, ultrices lectus. Cras condimentum neque a mauris egestas, vitae convallis mi pellentesque. Integer cursus tellus tortor, vel dictum odio placerat vel. Morbi vehicula leo nisl, et porttitor diam sagittis ut.',
+        rating:4,
+        similar:['jbjerlw', 'jbjerlx']
+    } ,
+
+    'jbjerlx':{
+        thumbnail:'https://placeimg.com/160/240/any',
+        title:'The Wizard of Oz',
+        showtimes:['16:45','18:35'] ,
+        img:'https://placeimg.com/640/480/any',
+        synopsis:'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis felis felis. Maecenas porta feugiat massa, at sagittis lectus facilisis vel. Vestibulum sit amet dolor maximus mauris posuere accumsan ut vulputate odio. Phasellus ut ipsum quis ex congue auctor. Proin id varius sapien, ut dictum lacus. Morbi ac ullamcorper nibh. Vivamus rhoncus est sed lorem tristique, id pharetra ante maximus. Mauris nunc justo, bibendum eu tempor id, ultrices eu ipsum. Curabitur placerat magna ut leo consequat, nec consectetur neque sodales. Curabitur eget risus tellus. Etiam hendrerit mi in dictum tincidunt. Nulla vulputate mi sem, id rutrum diam viverra quis. Cras eros lectus, dapibus nec lacinia vel, convallis a libero. Aliquam vel mauris faucibus, accumsan justo ut, ultrices lectus. Cras condimentum neque a mauris egestas, vitae convallis mi pellentesque. Integer cursus tellus tortor, vel dictum odio placerat vel. Morbi vehicula leo nisl, et porttitor diam sagittis ut.',
+        rating:4,
+        similar:['skjzbwn']
+    } ,
+
+    'skjzbwn':{
+        thumbnail:'https://placeimg.com/160/240/any',
+        title:'One Flew Over the Cuckoos Nest',
+        showtimes:['16:45','18:35'] ,
+        img:'https://placeimg.com/640/480/any',
+        synopsis:'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis felis felis. Maecenas porta feugiat massa, at sagittis lectus facilisis vel. Vestibulum sit amet dolor maximus mauris posuere accumsan ut vulputate odio. Phasellus ut ipsum quis ex congue auctor. Proin id varius sapien, ut dictum lacus. Morbi ac ullamcorper nibh. Vivamus rhoncus est sed lorem tristique, id pharetra ante maximus. Mauris nunc justo, bibendum eu tempor id, ultrices eu ipsum. Curabitur placerat magna ut leo consequat, nec consectetur neque sodales. Curabitur eget risus tellus. Etiam hendrerit mi in dictum tincidunt. Nulla vulputate mi sem, id rutrum diam viverra quis. Cras eros lectus, dapibus nec lacinia vel, convallis a libero. Aliquam vel mauris faucibus, accumsan justo ut, ultrices lectus. Cras condimentum neque a mauris egestas, vitae convallis mi pellentesque. Integer cursus tellus tortor, vel dictum odio placerat vel. Morbi vehicula leo nisl, et porttitor diam sagittis ut.',
+        rating:4,
+        similar:['jbjerlx', 'skjebwn']
+    } ,
+
+    'cbjerlx':{
+        thumbnail:'https://placeimg.com/160/240/any',
+        title:'Planet of the Apes',
+        showtimes:['16:45','18:35'] ,
+        img:'https://placeimg.com/640/480/any',
+        synopsis:'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis felis felis. Maecenas porta feugiat massa, at sagittis lectus facilisis vel. Vestibulum sit amet dolor maximus mauris posuere accumsan ut vulputate odio. Phasellus ut ipsum quis ex congue auctor. Proin id varius sapien, ut dictum lacus. Morbi ac ullamcorper nibh. Vivamus rhoncus est sed lorem tristique, id pharetra ante maximus. Mauris nunc justo, bibendum eu tempor id, ultrices eu ipsum. Curabitur placerat magna ut leo consequat, nec consectetur neque sodales. Curabitur eget risus tellus. Etiam hendrerit mi in dictum tincidunt. Nulla vulputate mi sem, id rutrum diam viverra quis. Cras eros lectus, dapibus nec lacinia vel, convallis a libero. Aliquam vel mauris faucibus, accumsan justo ut, ultrices lectus. Cras condimentum neque a mauris egestas, vitae convallis mi pellentesque. Integer cursus tellus tortor, vel dictum odio placerat vel. Morbi vehicula leo nisl, et porttitor diam sagittis ut.',
+        rating:4,
+        similar:['skjzbwn', 'jbjerlx']
+    } ,
+
+    'ckjzbwn':{
+        thumbnail:'https://placeimg.com/160/240/any',
+        title:'Psycho',
+        showtimes:['16:45','18:35'] ,
+        img:'https://placeimg.com/640/480/any',
+        synopsis:'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis felis felis. Maecenas porta feugiat massa, at sagittis lectus facilisis vel. Vestibulum sit amet dolor maximus mauris posuere accumsan ut vulputate odio. Phasellus ut ipsum quis ex congue auctor. Proin id varius sapien, ut dictum lacus. Morbi ac ullamcorper nibh. Vivamus rhoncus est sed lorem tristique, id pharetra ante maximus. Mauris nunc justo, bibendum eu tempor id, ultrices eu ipsum. Curabitur placerat magna ut leo consequat, nec consectetur neque sodales. Curabitur eget risus tellus. Etiam hendrerit mi in dictum tincidunt. Nulla vulputate mi sem, id rutrum diam viverra quis. Cras eros lectus, dapibus nec lacinia vel, convallis a libero. Aliquam vel mauris faucibus, accumsan justo ut, ultrices lectus. Cras condimentum neque a mauris egestas, vitae convallis mi pellentesque. Integer cursus tellus tortor, vel dictum odio placerat vel. Morbi vehicula leo nisl, et porttitor diam sagittis ut.',
+        rating:4,
+        similar:['jbjerlx', 'skjebwn', 'jbjerlw']
+    } ,
+}
